d0640af1a9b2c9b7775a3435886e1c7f01646cd1
bluehood <blue@lcm.mi.infn.it>
2016-04-21 11:53:26 +0200

    improve comments in EMNoisyOR

--- EMNoisyOR.py
+++ EMNoisyOR.py
@@ -12,7 +12,7 @@ import argparse
 
 np.set_printoptions(precision=14, suppress=True, threshold=10000)
 
-# Define parser
+# Define option parser
 parser = argparse.ArgumentParser()
 parser.add_argument('-s', '--samplesfile', required=True, dest="sFile",
                     help='the npy file containing the samples')
@@ -22,15 +22,17 @@ parser.add_argument('-t', '--tparamsfile', required=True, dest="tFile",
                     help='the npz file containing the true parameters')
 args = parser.parse_args()
 
+
 def pseudoLogJoint(Pi, W, hiddenVarConfs, samples, Ws):
-    """Takes the parameters and returns a matrix M[sample][hiddenVarConfs].
+    """Takes the parameters and returns a matrix M[hiddenVarConfs][sample].
     Each element of the matrix is the pseudo-log-joint probablity \
                     B*log(p(hiddenVarConf, sample))"""
 
     # prods_dc = 1 - Wbar_dc = prod{h}{1-W_dh*s_ch}
     prods = np.prod(Ws, axis=1)
     # logPriors_nc = sum{d}{y_nd*log(1/prods_dc - 1) + log(prods_dc)}
-    logPriors = np.dot(samples, np.log(1/prods - 1)) + np.sum(np.log(prods), axis=0)
+    logPriors = np.dot(samples, np.log(1/prods - 1)) + \
+                np.sum(np.log(prods), axis=0)
     # logHiddenVarProbs_c = sum{h}{hvc_ch}*log(Pi/(1-Pi))
     logHiddenVarProbs = np.sum(hiddenVarConfs, axis=1)*np.log(Pi/(1-Pi))
     # return pseudoLogJoints_cn
@@ -47,7 +49,9 @@ def meanPosterior(g, pseudoLogJoints, samples):
     the last axis represents the mean of g relative to each different
     sample.
     
-    The calculation performed is equivalent to np.dot(a, np.transpose(q))"""
+    The calculation performed is equivalent to np.dot(a, np.transpose(q))
+    wher q_cn are the posterior probabilities of each hidden variable
+    configuration c give sample n"""
 
     # Evaluate constants B_n by which we can translate pseudoLogJoints
     # TODO check grafically whether 20 is a good magic number
@@ -61,8 +65,8 @@ def meanPosterior(g, pseudoLogJoints, samples):
                 deltaSamples*np.exp(B))
 
 def logL(pseudoLogJoints, deltaSamples, nHiddenVars, Pi):
-    """Evaluate logL = logL - N*H*log(1-Pi).
-    LogL = sum{n}{log(prod{d}{delta(y_nd)} + \
+    """Evaluate log-likelihood logL
+    logL = sum{n}{log(prod{d}{delta(y_nd)} + \
             sum{c}{exp(pseudoLogJoints_cn)}} + N*H*log(1-Pi)"""
 
     return np.sum(np.log(deltaSamples + \
@@ -85,24 +89,27 @@ def signalHandler(signal, frame):
 # Set problem data
 nHiddenVars = args.nHiddenVars # Number of hidden variables assumed present
 samples = np.load(args.sFile)
+# deltaSamples_n is 1 if samples_nd == 0 for each d, 0 otherwise
 deltaSamples = np.array(~np.any(samples, axis=1), dtype=int)
-trueParams = np.load(args.tFile)
-eps = 1e-13 # minimum value allowed for synaptic weights (max is 1-eps)
+trueParams = np.load(args.tFile) # The ground-truth parameters
+eps = 1e-13 # Minimum value allowed for synaptic weights (max is 1-eps)
 
 # Create array containing all possible hidden variables configurations
 hiddenVarConfs = np.array([[0],[1]], dtype=int)
 for i in range(nHiddenVars-1):
     hiddenVarConfs = np.vstack([ np.hstack(([x,x], [[0],[1]]))
                                  for x in hiddenVarConfs ])
-# Remove the all-zero configuration. It is not required in the EM-algorithm.
+# Remove the all-zero configuration:
+# it is not required in the EM-algorithm and causes 0/0 calculations
 hiddenVarConfs = np.delete(hiddenVarConfs, 0, 0)
 
 # Initialise parameters to random values
+# TODO use smarter initial values for the parameters
 Pi = np.random.rand()
 W = np.random.rand(samples.shape[1], nHiddenVars) # W[dimSample][nHiddenVars]
 initW = W # save initial values of the parameters for visualisation purposes
 
-# Initialise parameters to the ground-truth values
+# Alternatively: initialise parameters to the ground-truth values
 # Pi = trueParams["Pi"]
 # W = trueParams["W"]
 
@@ -117,29 +124,35 @@ trueLogL = logL(pseudoLogJoint(trueParams["Pi"],
                  nHiddenVars,
                  trueParams["Pi"])
 
+# Ctrl-C now does not interrupts execution, just sets done = True
 signal.signal(signal.SIGINT, signalHandler)
 done = False
 counter = 0;
-logLs = ()
+logLs = () # The log-likelihoods evaluated at each step
 for i in range(100):
-    # E-step: evaluate pseudo-log-joint probabilities
-    # this is faster than W[:,None,:]*hvc[None,:,:]
+# Alternatively:
+# while not done:
+    # Ws are values needed both in the E and M-step, we evaluate them once here
+    # Ws_dhc = 1 - (W_dh * hiddenVarConfs_ch)
     Ws = 1 - np.einsum('ij,kj->ijk', W, hiddenVarConfs) 
+    # E-step: evaluate pseudo-log-joint probabilities
     pseudoLogJoints = pseudoLogJoint(Pi, W, hiddenVarConfs, samples, Ws)
 
     # Evaluate new likelihood and append it to the tuple
     logLs += (logL(pseudoLogJoints, deltaSamples, nHiddenVars, Pi),)
 
     # M-step
+    # Pi = sum{n}{<sum{h}{hiddenVarConfs_ch}>} / (N*H)
     Pi = np.sum(meanPosterior(np.sum(hiddenVarConfs, axis=1),
                               pseudoLogJoints,
                               samples)) / \
             (samples.shape[0]*nHiddenVars)
 
     # Wtilde_dhc = Prod{h'!=h}{1 - W_dj'*hiddenVarConfs_cj'}
+    # (Wtilde = np.fromfunction is much slower than a loop over j + np.stack)
     Wtilde = np.stack([ np.prod(np.delete(Ws, j, axis=1), axis=1) \
-            for j in range(Ws.shape[1]) ], axis=1)
-    denominators = 1 - Wtilde*Ws # faster than np.prod(Ws, axis=1)
+                        for j in range(Ws.shape[1]) ], axis=1)
+    denominators = 1 - Wtilde*Ws # faster than np.prod(Ws, axis=1) + newaxis
     denominators = (1 - denominators)*denominators
     D = np.einsum('ijk,kj->ijk', Wtilde, hiddenVarConfs) / denominators
     Dtilde = np.einsum('ijk,ki->ij',
@@ -154,13 +167,14 @@ for i in range(100):
         print "logL[" + str(counter) + "] = ", logLs[-1]
     counter += 1
 
-# Evaluate errors
+# Evaluate max difference with true values of a bars test
 smallval = np.min(trueParams["W"])
 bigval = np.max(trueParams["W"])
 smalldiff = np.abs(W[W<0.5] - smallval)
 bigdiff = np.abs(W[W>=0.5] - bigval) 
 Werror = np.max(np.append(smalldiff, bigdiff))
 
+# Save results to file and print out last parameter values
 filename = "l" + str(samples.shape[0])
 np.savez(filename, Pi=Pi, W=W,
          logLs=logLs, trueLogL=trueLogL, initW=initW)
