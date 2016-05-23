bd91d6fa46ca520a51489fba71ee8c12087c28ad
bluehood <blue@lcm.mi.infn.it>
2016-05-23 11:33:04 +0200

    clean-up plot_noisyOR

--- plot_noisyOR.py
+++ plot_noisyOR.py
@@ -5,31 +5,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
-from sys import exit
 
 # Define parser
 parser = argparse.ArgumentParser()
-parser.add_argument('-p', '--parFiles', dest='parFile',
-                    help="""The body of the names of the files containing the
-parameters. The data-points file will be set to
-'sPARFILE.npy'. The true parameters file will be set
-to 'tPARFILE.npz'. The learned parameters file will be set to 
-'lPARFILE.npz'. This is a commodity option to save
-typing. This option is overridden by the -s, -t and -l options
-if they are present.""")
-parser.add_argument('-t', '--truth', dest="truep", default="",
-                    help='the npz file containing the true parameters')
 parser.add_argument('-l', '--learned', dest="learnedp",
                     help='the npz file containing the learned parameters')
 parser.add_argument('-d', '--dps', dest="dps", default="",
                     help='the npy file containing the data-points')
 args = parser.parse_args()
-if not args.parFile:
-    if not (args.learnedp):
-        print """Please provide data-points, true and learned parameters
-filenames. The -p option can be used as a shorthand if filenames have the same
-body. Please use the -h option for more information"""
-        exit(1)
 
 def clear_axes(plot):
     axes = plot.get_axes()
@@ -38,42 +21,19 @@ def clear_axes(plot):
     axes.set_yticklabels([])
     axes.set_yticks([])
 
-
-def compare(A, B):
-    """Ad-hoc matrix comparison that uses a metric useful to
-list bars matrices in an ordered fashion"""
-    Ascore = np.sum(np.rint(A)*np.exp2(np.arange(A.size)).reshape(A.shape))
-    Bscore = np.sum(np.rint(B)*np.exp2(np.arange(B.size)).reshape(B.shape))
-    return cmp(Ascore,Bscore)
-
-if args.truep != "":
-    # Load true parameters
-    tp = np.load(args.truep or ('T' + args.parFile + '.npz'))
-    # Load log-likelihood and learned parameters
-    lp = np.load(args.learnedp or ('L' + args.parFile + '.npz'))
-    # Plot log-likelihood and true log-likelihood
-    plt.figure()
-    plt.plot(range(lp["logLs"].size),
-             lp["logLs"],
-             range(lp["logLs"].size),
-             tp["trueLogL"].repeat(lp["logLs"].size))
-    plt.title("True vs learned log-likelihood")
-    plt.xlabel("iterations")
-    plt.ylabel("log-likelihood")
-else:
-    # Load log-likelihood and learned parameters
-    lp = np.load(args.learnedp or ('L' + args.parFile + '.npz'))
-    # Plot log-likelihood and true log-likelihood
-    plt.figure()
-    plt.plot(range(lp["logLs"].size),
-             lp["logLs"])
-    plt.title("Learned log-likelihood")
-    plt.xlabel("iterations")
-    plt.ylabel("log-likelihood")
+# PRINT LOG-LIKELIHOOD
+# Load log-likelihood and learned parameters
+lp = np.load(args.learnedp or ('L' + args.parFile + '.npz'))
+# Plot log-likelihood and true log-likelihood
+plt.figure()
+plt.plot(range(lp["logLs"].size),
+         lp["logLs"])
+plt.title("Learned log-likelihood")
+plt.xlabel("iterations")
+plt.ylabel("log-likelihood")
 
 
-
-
+# PRINT INITIAL AND LEARNED WEIGHTS
 # Build grid of heat-maps to compare true and learned parameters
 nMatrices = lp["W"].shape[1]
 # matrices[0] is the initial matrices, matrices[1] is the learned ones,
@@ -84,7 +44,6 @@ matrices = [ np.transpose(lp["initW"]).reshape(lp["W"].shape[1],
              np.transpose(lp["W"]).reshape(lp["W"].shape[1],
                                            27,
                                            10) ]
-matrices[1] = np.array(sorted(matrices[1], cmp=compare))
 titles = ("Initial weights", "Learned weights", "True weights")
 plt.figure(figsize=(plt.rcParams['figure.figsize'][0],
                     plt.rcParams['figure.figsize'][1]*2))
@@ -98,6 +57,7 @@ for nMat in range(nMatrices):
             if nMat == 0:
                 plt.title(titles[i])
         
+# PRINT PI'S
 plt.figure()
 plot = plt.imshow(np.atleast_2d(lp["Pi"]), interpolation='none',
                   vmin=0., vmax=.5)
